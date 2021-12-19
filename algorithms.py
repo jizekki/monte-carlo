cf9861814bf2b9b34315239093c10d016119a04b
Jalal IZEKKI <jalal.izekki99@gmail.com>
2021-12-19 16:40:28 +0100

    started implementing an improved version of UCB

--- algorithms.py
+++ algorithms.py
@@ -4,16 +4,16 @@ from abc import ABC
 
 # Abstract class for all the algorithms
 class Algorithm(ABC):
-    def __init__(self, casino, initialCredits) -> None:
+    def __init__(self, casino, initialCredit) -> None:
         self._casino = casino
-        self._initialCredits = initialCredits
+        self._initialCredit = initialCredit
         self._runs = None
         self._rewards = None
         self.initialize()
 
     def initialize(self):
-        self._runs = np.zeros(self._casino.machines_count)
         self._rewards = np.zeros(self._casino.machines_count)
+        self._runs = np.zeros(self._casino.machines_count)
 
 
 # Abstract class for all glouton algorithms
@@ -21,11 +21,14 @@ class AbstractGlouton(Algorithm):
     def __init__(
         self,
         casino,
-        initialCredits=10000,
+        initialCredit=10000,
         testIterations=5,
     ) -> None:
-        super().__init__(casino, initialCredits)
+        super().__init__(casino, initialCredit)
         self._testIterations = testIterations
+
+    def initialize(self):
+        super().initialize()
 
     def do_abstract_glouton(self):
         self.initialize()
@@ -39,21 +42,19 @@ class AbstractGlouton(Algorithm):
 
 # Implementing the Glouton method
 class Glouton(AbstractGlouton):
-    def __init__(self, casino, testIterations=5, initialCredits=10000) -> None:
-        super().__init__(casino, initialCredits, testIterations)
+    def __init__(self, casino, testIterations=5, initialCredit=10000) -> None:
+        super().__init__(casino, initialCredit, testIterations)
 
     def do_glouton(self):
         super().do_abstract_glouton()
-        remaining_play_credits = (
-            self._initialCredits - self._testIterations * self._casino.machines_count
+        remaining_play_credit = (
+            self._initialCredit - self._testIterations * self._casino.machines_count
         )
-        best_machine_index = np.argmax(self._rewards)
-        while remaining_play_credits > 0:
-            self._rewards[best_machine_index] += self._casino.machines[
-                best_machine_index
-            ].play()
-            self._runs[best_machine_index] += 1
-            remaining_play_credits -= 1
+        best_machine = np.argmax(self._rewards)
+        while remaining_play_credit > 0:
+            self._rewards[best_machine] += self._casino.machines[best_machine].play()
+            self._runs[best_machine] += 1
+            remaining_play_credit -= 1
         return self._rewards.sum()
 
 
@@ -62,20 +63,20 @@ class EpsilonGlouton(AbstractGlouton):
     def __init__(
         self,
         casino,
-        initialCredits=10000,
+        initialCredit=10000,
         testIterations=5,
         epsilon=0.1,
     ) -> None:
-        super().__init__(casino, initialCredits, testIterations)
+        super().__init__(casino, initialCredit, testIterations)
         self._epsilon = epsilon
 
     def do_epsilon_glouton(self):
         super().do_abstract_glouton()
-        remaining_play_credits = (
-            self._initialCredits - self._testIterations * self._casino.machines_count
+        remaining_credit = (
+            self._initialCredit - self._testIterations * self._casino.machines_count
         )
         best_machine_index = np.argmax(self._rewards)
-        while remaining_play_credits > 0:
+        while remaining_credit > 0:
             if np.random.random() > self._epsilon:
                 self._rewards[best_machine_index] += self._casino.machines[
                     best_machine_index
@@ -87,22 +88,28 @@ class EpsilonGlouton(AbstractGlouton):
                     random_index
                 ].play()
                 self._runs[random_index] += 1
-            remaining_play_credits -= 1
+            remaining_credit -= 1
         return self._rewards.sum()
 
 
 # Implementing the UCB method
 class UCB(Algorithm):
-    def __init__(self, casino, initialCredits=10000) -> None:
-        super().__init__(casino, initialCredits)
+    def __init__(
+            self,
+            casino,
+            initialCredit=10000,
+            confidence=1
+    ) -> None:
+        super().__init__(casino, initialCredit)
         self._n = None
+        self.confidence = confidence
         self.initialize()
 
     def initialize(self):
         super().initialize()
         self._n = 0
 
-    def _maximal_reward_arg(self, c=1):
+    def _maximal_reward_arg(self):
         """
         Gets the machine with the highest reward so far.
         If a machine that has never been tried is found, it will be returned
@@ -114,16 +121,16 @@ class UCB(Algorithm):
                 return a
             else:
                 delta = np.sqrt(2 * np.log(self._n) / self._runs[a])
-                reward = self._rewards[a] + c * delta
+                reward = self._rewards[a] + self.confidence * delta
                 if reward > maximal_reward:
                     maximal_reward = reward
                     arg = a
         return arg
 
-    def do_one_step_ucb(self, c=1):
-        if self._n >= self._initialCredits:
+    def do_one_step_ucb(self):
+        if self._n >= self._initialCredit:
             return None, self._n
-        arg = self._maximal_reward_arg(c)
+        arg = self._maximal_reward_arg()
         reward = self._casino.machines[arg].play()
         self._rewards[arg] = (self._runs[arg] * self._rewards[arg] + reward) / (
             self._runs[arg] + 1
@@ -132,11 +139,55 @@ class UCB(Algorithm):
         self._n += 1
         return reward, self._n - 1
 
-    def do_ucb(self, c=1):
+    def do_ucb(self):
         self.initialize()
         v = 0
-        for i in range(self._initialCredits):
-            reward, _ = self.do_one_step_ucb(c)
+        for i in range(self._initialCredit):
+            reward, _ = self.do_one_step_ucb()
             v += reward
-        assert self.do_one_step_ucb(c)[0] is None
+        assert self.do_one_step_ucb()[0] is None
+        assert self._runs.sum() == self._initialCredit
+        return v
+
+
+# Implementing the UCB method
+class ImprovedUCB(Algorithm):
+    def __init__(
+            self,
+            casino,
+            initialCredit=10000,
+            confidence=1
+    ) -> None:
+        super().__init__(casino, initialCredit)
+        self._n = None
+        self.confidence = confidence
+        self.initialize()
+
+    def initialize(self):
+        super().initialize()
+        self._n = 0
+
+    def _maximal_reward_arg(self):
+        return np.argmax(self._rewards + self.confidence * np.sqrt((np.log(self._n + 1)) / (self._runs + 1)))
+
+    def do_one_step_ucb(self):
+        if self._n >= self._initialCredit:
+            return None, self._n
+        arg = self._maximal_reward_arg()
+        reward = self._casino.machines[arg].play()
+        self._rewards[arg] = (self._runs[arg] * self._rewards[arg] + reward) / (
+            self._runs[arg] + 1
+        )
+        self._runs[arg] += 1
+        self._n += 1
+        return reward, self._n - 1
+
+    def do_ucb(self):
+        self.initialize()
+        v = 0
+        for i in range(self._initialCredit):
+            reward, _ = self.do_one_step_ucb()
+            v += reward
+        assert self.do_one_step_ucb()[0] is None
+        assert self._runs.sum() == self._initialCredit
         return v
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from casino import Casino
-from algorithms import Glouton, EpsilonGlouton, UCB
+from algorithms import Glouton, EpsilonGlouton, UCB, ImprovedUCB
 
 if __name__ == "__main__":
 
@@ -7,10 +7,12 @@ if __name__ == "__main__":
     glouton = Glouton(casino)
     epsilonGlouton = EpsilonGlouton(casino)
     ucb = UCB(casino)
+    improved_ucb = ImprovedUCB(casino)
 
     print("Glouton reward: {}".format(glouton.do_glouton()))
     print("Epsilon Glouton reward: {}".format(epsilonGlouton.do_epsilon_glouton()))
     print("UCB reward: {}".format(ucb.do_ucb()))
+    print("Improved UCB reward: {}".format(improved_ucb.do_ucb()))
     print(
         "best choice reward : {:.2f}".format(
             sum(casino.real_best_choice().play() for _ in range(10000))
